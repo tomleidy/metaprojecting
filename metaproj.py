eacff0b4c08e6b122fb5b6bdaa4584b9455f2e03
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-10-12 20:25:03 -0400

    feat: iterates and hashes files

---
+++ metaproj.py
@@ -0,0 +1,76 @@
+import os
+import hashlib
+import subprocess
+import json
+
+CODING_DIR = os.path.dirname(os.path.expanduser("~/Coding/"))
+COMMAND_DETERMINE_REPO_ROOT = ("git", "rev-parse", "--show-toplevel")
+COMMAND_DETERMINE_GIT_STATUS = ("git", "status", "--porcelain")
+
+PROJECT_INDICATOR_FILES = set({"requirements.txt", "Pipfile", "Pipfile.lock", "pyproject.toml", "setup.py", "package.json", "yarn.lock", "tsconfig.json", "webpack.config.js", "babel.config.js", ".babelrc", ".eslintrc", ".eslintignore", "node_modules", "venv", "CMakeLists.txt", "Makefile", ".git", "Dockerfile", ".env", "__pycache__", ".venv", "venv-mac", "venv-win"})
+
+FOLDERS_DO_NOT_RECURSE_INTO = set({"node_modules", "venv", "__pycache__", ".git", "build", "dist", "docs" })
+DELETE_FILES = set({".DS_Store"})
+
+files_and_directories = []
+FILE_FOLDER_METADATA = ["timestamp","sha256sum","filename","absolute_path","relative_path","size","is_project", "git_status", "subdirectories"]
+
+
+# TODO: Add support for detecting the root of a repo
+# TODO: Count subdirectories
+# TODO: Folders to not recurse into
+# TODO: Implement project detection
+# TODO: Implement ignore files
+# TODO: def get_symlink_metadata
+
+def get_hash(file_path) -> str:
+    with open(file_path, "rb") as file:
+        hash = hashlib.file_digest(file, "sha256").hexdigest()
+    return hash
+
+
+
+def get_file_metadata(file_path):
+    if os.path.islink(file_path):
+        return None
+    # get relative path from CODING_DIR
+    relative_path = os.path.relpath(os.path.abspath(file_path), CODING_DIR)
+    # get timestamp
+    timestamp = os.path.getmtime(file_path)
+    # hash file
+    sha256sum = get_hash(file_path)
+    # get filename
+    filename = os.path.basename(file_path)
+    size = os.path.getsize(file_path)
+    subdirectories = 0
+    is_project = False # most likely, a lone file not a project
+    git_status = subprocess.check_output(COMMAND_DETERMINE_GIT_STATUS + (file_path,)).decode("utf-8")
+    file_dict = {key: value for key, value in locals().items() if key in FILE_FOLDER_METADATA}
+    #print(file_dict)
+    return file_dict
+
+
+def iterate_through_directory(directory):
+    segment_count = []
+    for root, dirs, files in os.walk(directory,topdown=True):
+        dirs[:] = [d for d in dirs if d not in FOLDERS_DO_NOT_RECURSE_INTO]
+        for file in files:
+            if file in DELETE_FILES:
+                print(f"removing {file}")
+                #os.remove(file)
+            file_path = os.path.join(root, file)
+            file_dict = get_file_metadata(file_path)
+            if file_dict:
+                files_and_directories.append(file_dict)
+
+        #for dir in dirs:
+            #dir_path = os.path.join(root, dir)
+            #dir_dict = get_file_metadata(dir_path)
+            #files_and_directories.append(dir_dict)
+    print(json.dumps(files_and_directories, indent=4))
+    #print(max(segment_count))
+
+
+
+if __name__ == "__main__":
+    iterate_through_directory(CODING_DIR)
