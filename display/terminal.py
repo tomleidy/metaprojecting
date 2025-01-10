c834334e9ceb7b42f206de4c4f5b59d0b3d32a72
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2025-01-09 21:10:47 -0500

    feat: add full border to display, update on terminal resize

--- display/terminal.py
+++ display/terminal.py
@@ -1,4 +1,5 @@
 # from localconfig.path import CODING_DIR
+import signal
 from blessed import Terminal
 
 APP_TITLE = "Epi-Project[ing]"
@@ -10,13 +11,16 @@ class Display:
     def __init__(self):
         self.titlebar_text = ""
         self.statusbar_text = ""
-        self._display_border()
+        # regenerate display if terminal size changes
+        # this is going to have to become the more general method when we've figured out
+        # displaying category/project lists, scrollbars, etc.
+        signal.signal(signal.SIGWINCH, self._display_border)
         self._wait_for_input()
 
     def _send_update(self, update):
         pass
 
-    def _display_border(self):
+    def _display_border(self, sig=None, action=None):
         """Display and update titlebar"""
         width = term.width
         title = " " + APP_TITLE + " "
@@ -24,7 +28,7 @@ class Display:
         title_end = title_start + len(title)
         line = '╔' + '═' * (title_start - 1) + title + '═' * (width - title_end - 1) + '╗'
 
-        with term.location(y=0), term.hidden_cursor():
+        with term.location(y=0):
             print(term.home + term.on_bright_blue + term.clear, end='')
             print(line)
             for y in range(1, term.height-1):
@@ -37,6 +41,7 @@ class Display:
 
     def _wait_for_input(self):
         with term.cbreak(), term.hidden_cursor():
+            self._display_border()
             val = ''
             while val.lower() != 'q':
                 val = term.inkey(timeout=3)
